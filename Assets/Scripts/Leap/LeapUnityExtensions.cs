b8184013d82b88d6eff5453e9140afe2ee46c68e
Brennon Bortz <brennon@brennonbortz.com>
2013-03-18 22:36:21 -0400

    Document LeapUnityExtensions.cs

--- Assets/Scripts/Leap/LeapUnityExtensions.cs
+++ Assets/Scripts/Leap/LeapUnityExtensions.cs
@@ -16,29 +16,37 @@ namespace Leap {
 	// Leap coordinates are in cm, so the .02f scaling factor means 1cm of hand motion = .02m scene motion
 	public static class UnityVectorExtension
 	{
+		// Change scale and offset here, if desired.
 		public static Vector3 InputScale = new Vector3(0.02f, 0.02f, 0.02f);
 		public static Vector3 InputOffset = new Vector3(0,0,0);
 		
-		//For Directions
+		// For Directions
 		public static Vector3 ToUnity(this Vector lv)
 		{
 			return FlippedZ(lv);
 		}
-		//For Acceleration/Velocity
+		
+		// For Acceleration/Velocity
 		public static Vector3 ToUnityScaled(this Vector lv)
 		{
 			return Scaled(FlippedZ( lv ));
 		}
-		//For Positions
+		
+		// For Positions
 		public static Vector3 ToUnityTranslated(this Vector lv)
 		{
 			return Offset(Scaled(FlippedZ( lv )));
 		}
 		
+		// Change sign of z-axis.
 		private static Vector3 FlippedZ( Vector v ) { return new Vector3( v.x, v.y, -v.z ); }
+		
+		// Apply InputScale to vector.
 		private static Vector3 Scaled( Vector3 v ) { return new Vector3( v.x * InputScale.x,
 																		 v.y * InputScale.y,
 																		 v.z * InputScale.z ); }
+		
+		// Shift all vector values by InputOffset.
 		private static Vector3 Offset( Vector3 v ) { return v + InputOffset; }
 	}
 }
