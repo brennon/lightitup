22c0abfb9b200a8a4f1c2d7cffd58158368c2a15
Brennon Bortz <brennon@brennonbortz.com>
2013-03-23 23:25:01 -0400

    Randomization of conditions

--- Assets/GUI/MainGUI.cs
+++ Assets/GUI/MainGUI.cs
@@ -3,23 +3,71 @@ using System.Collections;
 
 public class MainGUI : MonoBehaviour {
 	
-	public string subjectID = "Enter ID";
-	public Rect subjectIDRect = new Rect();
-
-	void OnGUI () {
-		// Make a background box
-		// GUI.Box(new Rect(10,10,100,90), "Loader Menu");
-		
-		subjectID = GUI.TextArea(subjectIDRect, subjectID, 5);
-
-		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
-		if(GUI.Button(new Rect(20,40,80,20), "Training")) {
-			Application.LoadLevel("Training");
+	public string subjectID = "ID";
+	public int[] conditionList = new int[6];
+	public int currentTrial = -1;
+	
+	void Start() {
+		// Randomize trials
+		int[,] conditions = {{0,1},{2,3},{4,5}};
+		System.Random r = new System.Random();
+		for (int i = 2; i > 0; i--) {
+			int swapIndex = r.Next(i + 1);
+			int currentLeft = conditions[swapIndex,0];
+			int currentRight = conditions[swapIndex,1];
+			conditions[swapIndex,0] = conditions[i,0];
+			conditions[swapIndex,1] = conditions[i,1];
+			conditions[i,0] = currentLeft;
+			conditions[i,1] = currentRight;
 		}
+		
+		// Randomize mouse/Leap order
+		for (int i = 2; i >= 0; i--) {
+			int swapIndex = r.Next(2);
+			if (swapIndex == 0) {
+				int current = conditions[i,1];
+				conditions[i,1] = conditions[i,0];
+				conditions[i,0] = current;
+			}
+		}
+		
+		// Save ordering to conditionList
+		for (int i = 0; i < 3; i++) {
+			conditionList[i*2] = conditions[i,0];
+			conditionList[i*2+1] = conditions[i,1];
+		}
+		
+		DontDestroyOnLoad(this);
+	}
 
-		// Make the second button.
-		if(GUI.Button(new Rect(20,70,80,20), "Begin Experiment")) {
-			Application.LoadLevel("UnitySandbox");
+	void OnGUI() {
+		// Store typed text in subjectID
+		subjectID = GUI.TextArea(new Rect(Screen.width/2 - 75,Screen.height/2,100,20), subjectID, 5);
+		
+		// If this is the Welcome scene
+		if (UnityEditor.EditorApplication.currentScene == "Assets/Scenes/Welcome.unity") {			
+			GUIStyle labelStyle = new GUIStyle();
+			labelStyle.alignment = TextAnchor.MiddleCenter;
+			labelStyle.normal.textColor = Color.white;
+			GUI.Label(new Rect(Screen.width/2, Screen.height/2 - 20, 0, 0), "Welcome! Please enter a subject ID and click 'Begin':",labelStyle);
+			// Print ID entry widgets
+			if(GUI.Button(new Rect(Screen.width/2 + 30,Screen.height/2,50,20), "Begin")) {
+				Application.LoadLevel("Training");
+			}
+		}
+		
+		// If this is the Training scene
+		if (UnityEditor.EditorApplication.currentScene == "Assets/Scenes/Welcome") {
+		}
+	}
+	
+	void AdvanceLevel() {
+		if (currentTrial++ < 6) {
+			if (conditionList[currentTrial] % 2 == 0) {
+				Application.LoadLevel("Trial - Mouse");
+			} else {
+				Application.LoadLevel("Trial - Leap");
+			}
 		}
 	}
 }
