cdf467f0892e0f08541dceaa8572ffed8d797ade
Brennon Bortz <brennon@brennonbortz.com>
2013-04-15 08:22:53 -0400

    Comment print in ReferenceGUI.cs

--- Assets/GUI/ReferenceGUI.cs
+++ Assets/GUI/ReferenceGUI.cs
@@ -23,8 +23,8 @@ public class ReferenceGUI : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (toolbarInt != currentTrial) {
-		//	toolbarInt = currentTrialNumber;
-			Debug.Log ("loading level " + "Reference" + toolbarInt);
+			// toolbarInt = currentTrialNumber;
+			// Debug.Log ("loading level " + "Reference" + toolbarInt);
 			Application.LoadLevel("Reference" + toolbarInt);
 		}
 	}
